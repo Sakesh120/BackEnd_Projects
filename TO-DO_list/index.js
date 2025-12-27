97f83eaa2541e8a7e1f026047396011ccf0874a2
Sakesh120 <sakeshchopkar927@gmail.com>
2025-12-27 23:02:50 +0530

    CRUD Poeration on ToDo

--- TO-DO_list/index.js
+++ TO-DO_list/index.js
@@ -1,22 +1,68 @@
-import express from "express";
+import express, { urlencoded } from "express";
+import { MongoClient, ObjectId } from "mongodb";
 import path from "path";
 
 const app = express();
-
+app.use(urlencoded({ extended: false }));
 const publicPath = path.resolve("public");
 app.use(express.static(publicPath));
 
 app.set("view engine", "ejs");
-app.get("/", (req, res) => {
-  res.render("list");
+
+const dbName = "node_project";
+const collectionName = "todo";
+const url = "mongodb://localhost:27017";
+const client = new MongoClient(url);
+
+const connection = async () => {
+  const connect = await client.connect();
+  return await connect.db(dbName);
+};
+
+app.get("/", async (req, res) => {
+  const db = await connection();
+  const collection = db.collection(collectionName);
+  const result = await collection.find().toArray();
+  res.render("list", { result });
 });
 app.get("/add", (req, res) => {
   res.render("taskAdd");
 });
-app.post("/submit", (req, res) => {
-  res.redirect("/");
+
+app.post("/add", async (req, res) => {
+  const db = await connection();
+  const collection = db.collection(collectionName);
+  const result = await collection.insertOne(req.body);
+
+  result ? res.redirect("/") : res.redirect("/add");
 });
-app.get("/update", (req, res) => {
-  res.render("taskUpdate");
+
+app.get("/delete/:id", async (req, res) => {
+  const id = req.params.id;
+  const db = await connection();
+  const collection = db.collection(collectionName);
+  const result = await collection.deleteOne({ _id: new ObjectId(id) });
+  result ? res.redirect("/") : res.send("some error");
 });
+
+app.get("/update/:id", async (req, res) => {
+  const id = req.params.id;
+  const db = await connection();
+  const collection = db.collection(collectionName);
+  const result = await collection.findOne({ _id: new ObjectId(id) });
+  result ? res.render("taskUpdate", { result }) : res.send("some error");
+});
+
+app.post("/update/:id", async (req, res) => {
+  const id = req.params.id;
+  const db = await connection();
+  const collection = db.collection(collectionName);
+  const filter = { _id: new ObjectId(id) };
+  const updatedData = {
+    $set: { title: req.body.title, massege: req.body.massege },
+  };
+  const result = await collection.updateOne(filter, updatedData);
+  result ? res.redirect("/") : res.send("some error");
+});
+
 app.listen(3200);
